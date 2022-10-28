654985338ab4b697033c1fbb9250342802cad148
Jesse-Viterbo <115576250+Jesse-Viterbo@users.noreply.github.com>
2022-10-28 18:25:54 +0000

    Add files via upload

--- empPoulin.js
+++ empPoulin.js
@@ -1,37 +1,45 @@
 /*Store Employee Name and ID Number */
 class Employee {
-    constructor(empName, empID) {
-        this.empName = empName;
-        this.empID = empID;
+    constructor(empName, empId) {
+        this._empName = empName;
+        this._empId = empId;
     }
     get empName() {
-        return(this.empName);
+        return(this._empName);
     }
-    get empID() {
-        return(this.empID);
+    get empId() {
+        return(this._empId);
     }
 }
 /*Subclass of Employee, keep data for shift number and hourly pay rate */
 class ProductionWorker extends Employee {
-    constructor(shiftNum, payRate) {
-        this.shiftNum = shiftNum;
-        this.payRate = payRate;
+    constructor(empName, empId, shiftNum, payRate) {
+        super(empName, empId);
+        this._shiftNum = shiftNum;
+        this._payRate = payRate;
     }
     get shiftNum() {
-        return(this.shiftNum);
+        return this._shiftNum;
     }
     get payRate() {
-        return(this.payRate);
+        return this._payRate;
     }
 }
 
 function calculate() {
-    let empName =  document.getElementById('empName');
-    let empId = document.getElementById('id');
-    let shiftNum = document.getElementById('shiftNum');
-    let payRate = document.getElementById('payRate');
+    let empName =  document.getElementById('empName').value;
+    let empId = document.getElementById('empId').value;
+    let shiftNum = document.getElementById('shiftNum').value;
+    let payRate = document.getElementById('payRate').value;
     
+    
+    const prodWorker = new ProductionWorker(empName, empId, shiftNum, payRate);
+    //https://natclark.com/tutorials/javascript-formatting-currencies/
+    const payRateFormatted = new Intl.NumberFormat(`en-US`, {
+        currency: `USD`,
+        style: 'currency',
+    }).format(prodWorker.payRate);
 
-    output = "Name: " + "<br>" + "ID: " + "<br>" + "Shift: " + "<br>" + "Hourly Pay Rate: ";
+    output = "Name: " + prodWorker.empName + "<br>" + "ID: " + prodWorker.empId + "<br>" + "Shift: " + prodWorker.shiftNum + "<br>" + "Hourly Pay Rate: " + payRateFormatted;
     document.getElementById('formResults').innerHTML = output;
 }
